18c4a1bfaadd32c906a95aa8a13620fa8904cb74
pelicanpaul <thepaullyons@gmail>
2015-06-08 18:17:58 -0700

    content added, swatches, css and js

--- abstract/js/site.js
+++ abstract/js/site.js
@@ -7,4 +7,33 @@ $(document).ready(function(){
         $(this).html($(this).html().replace(/\s([^\s<]+)\s*$/,'&nbsp;$1'));
     });
 
+    $('.swatch').on('click', function(){
+
+        $('.swatch').each(function(){
+            $(this).html('');
+        });
+
+        var colorId = $(this).data('colorid');
+        var thisHtml = $(this).html();
+
+        var thisHtmlLen = thisHtml.length;
+
+        if ($(this).hasClass('active') == true){
+            $(this).html('');
+            $('.swatch').each(function(){
+                $(this).removeClass('active');
+            });
+        } else {
+            $(this).html('<div class="swatch-tt"><div class="arrow-box swatch-' + colorId + '-over"></div></div>');
+            $('.swatch-tt').fadeIn();
+            $(this).addClass('active');
+        }
+
+    });
+
+    $('#button-swatches').click(function(){
+        $('#container-swatches').toggle();
+
+    });
+
 });
